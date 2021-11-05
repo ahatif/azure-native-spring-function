8c367b8686954ae9173ccabf2c3833618d11fff9
Abbas Hatif <abbashatif@yahoo.com>
2021-11-06 00:43:11 +0100

    Fixed test

--- src/main/java/com/example/HelloFunction.java
+++ src/main/java/com/example/HelloFunction.java
@@ -11,6 +11,6 @@ import java.util.function.Function;
 public class HelloFunction implements Function<Mono<User>, Mono<Greeting>> {
 
     public Mono<Greeting> apply(Mono<User> mono) {
-        return mono.map(user -> new Greeting("Salam/Hoi, " + user.getName() + "!\n"));
+        return mono.map(user -> new Greeting("Salam, " + user.getName() + "!\n"));
     }
 }
